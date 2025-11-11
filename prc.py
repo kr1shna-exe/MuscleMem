ae20850ff6eb5b6517e208e3c3d26c362f30a9aa
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-11-11 10:53:02 +0530

    .

--- prc.py
+++ prc.py
@@ -127,3 +127,55 @@ clone = deep_copy(Input)
 clone["user"]["scores"][2]["bonus"] = 99
 print(clone["user"]["scores"][2]["bonus"])
 print(Input["user"]["scores"][2]["bonus"])
+
+Input = "aaabbccccdaa"
+Output = a3b2c4d1a2 # If not shorter than Input then return Input
+
+def compress_string(s: str) -> str:
+    res, i = '', 0
+    while i < len(s):
+        ct = 1
+        while i + 1 < len(s) and s[i + 1] == s[i]:
+            ct += 1
+            i += 1
+        res += s[i] + str(ct)
+        i += 1
+    return res if len(res) < len(s) else s
+
+print(compress_string(Input))
+
+def compress_string(s: str) -> str:
+    res = ''
+    ct = 1
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            ct += 1
+        else:
+            res += s[i] + str(ct)
+            ct = 1
+    return res if res < s else s
+print(compress_string(Input))
+
+Output = 
+""                  → True
+"()[]{}"            → True
+"( [ { } ] )"      → True   (ignore spaces if you want)
+"(]"                → False
+"([)]"              → False
+"((("               → False
+")("                → False
+
+def is_valid_brackets(s: str) -> bool:
+    if not s:
+        return True
+    stack = []
+    pairs = { ')' : '(', '}' : '{', ']' : '['}
+    for i in range(len(s)):
+        if s[i] in '({[':
+            stack.append(s[i])
+        elif s[i] in pairs:
+            if pairs[s[i]] != stack.pop():
+                return False
+    return True if not stack else False
+
+print(is_valid_brackets("a+(b*c)-[d]"))
